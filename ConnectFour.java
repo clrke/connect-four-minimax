5a77cbba5c8bbf0a8ad30432965129e2b18d5915
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2018-05-06 23:50:18 +0800

    Reorder imports

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,8 +1,8 @@
-import javax.swing.*;
-import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import javax.swing.*;
+import javax.swing.border.*;
 import java.util.*;
 
 enum Piece
